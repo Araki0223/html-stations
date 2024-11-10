592355ab1f5b45ad6c2aa7dad9a0285c45998cd8
Loukas <davidsizu5051r2@gmail.com>
2024-11-10 16:41:33 +0900

    station15終わった_2024-11-10

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -6,11 +6,27 @@ async function getData() {
     { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
   ];
-  const result = await test()
-  return await result
+  const result = await test(userList);
+  return await result;
+
+  // const results = await test();
+  // console.log("getData - results:",results);
+  // return results;
 }
 
-function test() {
-  return
+function test(userList) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      // const userList = [
+      //   { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
+      //   { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
+      // ];
+      resolve(
+        userList.map(user => ({
+          full_name: user.family_name + ' ' + user.first_name
+        }))
+      );
+    }, 3000);
+  });
 }
 
